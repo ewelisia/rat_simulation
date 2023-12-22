ff867020a826360da2adebc200db844cb67af86d
Ewelisia <ewelisia@LAPTOP-6G0C4VT3.localdomain>
2023-12-22 11:22:57 +0100

    rat eat food

--- rat.py
+++ rat.py
@@ -9,7 +9,7 @@ class Rat:
     Represents a rat with health, position, age, and ability to move, reproduce, and die.
     """
 
-    def __init__(self, health, x, y, age=0):
+    def __init__(self, health, x, y, age=0, eat_pace=10, has_food=True):
         """
         Initializes a new rat with specified health, x and y coordinates, and age.
 
@@ -22,6 +22,8 @@ class Rat:
         self.health = health  # Health can be set here or initialized randomly if needed.
         self.x = x
         self.y = y
+        self.eat_pace=eat_pace
+        self.has_food=has_food
 
     def move(self, a, b, allowed_moves):
         """
@@ -63,7 +65,11 @@ class Rat:
         :return: 1 if the rat dies, otherwise 0.
         """
         probability = min(1, self.age / 10)
+        if not self.has_food:
+            return 1
         return 1 if random.random() < probability else 0
+    
+    
 
     def place(self):
         """
@@ -76,6 +82,15 @@ class Rat:
     def update_age(self):
         """ Increments the age of the rat by 1. """
         self.age += 1
+
+    def eat(self, food_amount):
+        if self.eat_pace<food_amount:
+            return food_amount-self.eat_pace
+        else:
+            self.has_food==False
+            return 0 
+        
+
 
 
 class Grid:
@@ -93,6 +108,7 @@ class Grid:
         self.a = a
         self.b = b
         self.rat_list = []  # Store the rats on the grid.
+        self.food_matrix=np.random.randint(0, 100, size=(self.a+1, self.b+1))
 
     def add(self, rat):
         """ Adds a rat to the grid. """
@@ -102,7 +118,7 @@ class Grid:
         """
         Initializes the grid with a random number of rats.
         """
-        num = random.randint(5, 15)
+        num = random.randint(30, 40)
         positions=[]
         
         for _ in range(num):
@@ -113,11 +129,13 @@ class Grid:
                 self.add(new_rat)
                 positions.append((x,y))
 
+        
+
     def define_possible_moves(self, rat):
         rat_positions = [other_rat.place() for other_rat in self.rat_list]
         pos_moves = self.possible_move(rat.x, rat.y)
-        pos_moves_rat = [move for move in pos_moves if move not in rat_positions]
-        return pos_moves_rat
+        allowed_moves = [move for move in pos_moves if move not in rat_positions]
+        return allowed_moves
 
     def run_step(self):
         """
@@ -126,13 +144,17 @@ class Grid:
         rat_to_die = []
         for i, rat in enumerate(self.rat_list):
             rat.update_age()
-            pos_moves_rat = self.define_possible_moves(rat)
-            rat.move(self.a, self.b, pos_moves_rat)
+            allowed_moves = self.define_possible_moves(rat)
+            rat.move(self.a, self.b, allowed_moves)
+            x, y = rat.place()
+            available_food=self.food_matrix[x][y]
+            remaining_food=rat.eat(available_food)
+            self.food_matrix[x][y]=remaining_food
 
             if rat.reproduction() == 1:
                 reproducted_rat_location=self.define_possible_moves(rat)
                 if len(reproducted_rat_location)>0:
-                    new_rat = Rat("good", pos_moves_rat[0][0], pos_moves_rat[0][1])
+                    new_rat = Rat("good", allowed_moves[0][0], allowed_moves[0][1])
                     self.add(new_rat)
 
             if rat.die() == 1:
@@ -219,7 +241,8 @@ class Grid:
 grid_console = Grid()
 grid_console.initialize()
 print(grid_console.position())
-for n in range(10):
+for n in range(30):
     grid_console.run_step()
     grid_console.display_rats(n)
     print(len(grid_console.rat_list))
+    print(grid_console.food_matrix)
